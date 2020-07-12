f11313f1d767da49733cbe77bd4468055fd9dbe1
from0to1-nk <skykerch@live.com>
2020-07-13 00:48:29 +0300

    addet sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
 $(function () {
     var mixer = mixitup('.products__box');
+    $('.featured__slider').slick({
+            prevArrow: '<button type = "button" class = "featured__btn featured__btn-prev"> <img src = "images/icons/chevron-left.svg" class="chevron"></button>',
+            nextArrow: '<button type = "button" class = "featured__btn featured__btn-next"> <img  class="chevron" src = "images/icons/chevron-right.svg"></button>',
+            slidesToScroll: 1,
+            slidesToShow: 1,
+        }
 
+    );
+    $('.followers__slider').slick({
+            prevArrow: '<button type = "button" class = "followers__btn followers__btn-prev"> <img src = "images/icons/chevron-left.svg" class="chevron"></button>',
+            nextArrow: '<button type = "button" class = "followers__btn followers__btn-next"> <img  class="chevron" src = "images/icons/chevron-right.svg"></button>',
+            slidesToScroll: 3,
+            slidesToShow: 3,
+        }
 
+    );
+    $(".rateYo").rateYo({
+        rating: 5,
+        starWidth: "15px",
+        readOnly: true
+    });
 
 
 
