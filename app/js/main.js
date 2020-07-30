17022ed1235e6fe429a4250bf0a4129679fa6553
from0to1-nk <skykerch@live.com>
2020-07-30 10:47:59 +0300

    addet home page

--- app/js/main.js
+++ app/js/main.js
@@ -9,20 +9,37 @@ $(function () {
 
     );
     $('.followers__slider').slick({
-            prevArrow: '<button type = "button" class = "followers__btn followers__btn-prev"> <img src = "images/icons/chevron-left.svg" class="chevron"></button>',
+            prevArrow: '<button type = "button" class = "followers__btn followers__btn-prew"> <img  class="chevron" src = "images/icons/chevron-left.svg"></button>',
             nextArrow: '<button type = "button" class = "followers__btn followers__btn-next"> <img  class="chevron" src = "images/icons/chevron-right.svg"></button>',
             slidesToScroll: 3,
             slidesToShow: 3,
+
         }
 
     );
+    $('.feedback__slider').slick({
+        prevArrow: '<button type="button" class = "feedback__btn feedback__btn-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button type = "button" class = "feedback__btn feedback__btn-next lnr lnr-chevron-right"></button>',
+        slidesToScroll: 2,
+        slidesToShow: 2,
+    });
+
     $(".rateYo").rateYo({
-        rating: 5,
-        starWidth: "15px",
-        readOnly: true
+        rating: 3.5,
+        starWidth: "13px",
+        readOnly: true,
+        halfStar: true,
+        spacing: "5px",
     });
 
 
+    $(".featured-rateYo").rateYo({
+        rating: 3.5,
+        starWidth: "16px",
+        readOnly: true,
+        halfStar: true,
+        spacing: "4px",
+    });
 
 
 })
